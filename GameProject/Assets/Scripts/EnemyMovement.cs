2015e26c5165b46ad78a18b4cdde4ead580cde60
maxikfu <maksimka2211@gmail.com>
2017-10-01 22:04:12 -0500

    Game 2.5D freez on axis Y
    1. Player Movment in XZ plate
    2. Enemy movment, following player
    3. Enemy damages player, then close enough
    4. Player health UI, slidebar, screen flashing red when damaged
    
    To do:
    1. Creating enemys non-stop
    2. Scoring board.
    3. Player attack script
    Also needed:
    1.Player state (animations): Running, Dead, Shooting
    2.Enemy state(animations): Running, Dead, Effects when get shot
    3. Audio
    4.Camera shaking when shooting etc

---
+++ GameProject/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    private UnityEngine.AI.NavMeshAgent nav; //Reference to the nav mesh agent
+    //PlayerHealth playerHealth;      // Reference to the player's health.
+    //EnemyHealth enemyHealth;        // Reference to this enemy's health.
+                                   
+    void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	// Update is called once per frame
+	void Update () {
+        // If the enemy and the player have health left...
+        //if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //{
+            // ... set the destination of the nav mesh agent to the player.
+            nav.SetDestination(player.position);
+        //}
+        // Otherwise...
+        //else
+        //{
+            // ... disable the nav mesh agent.
+           // nav.enabled = false;
+        //}
+    }
+}
