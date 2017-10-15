4e07fcfe9a12d33297f5c2cf52196eb495bb9f62
sul01 <sulrend@yahoo.com>
2017-10-15 01:37:35 -0500

    Recovery Pick Up

---
+++ GameProjectWorking/Assets/Scripts/healthPickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthPickUp : MonoBehaviour {
+
+    public int heal = 20;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            other.GetComponent<PlayerHealth>().currentHealth += heal;
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
