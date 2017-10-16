2d49358b144c936a9944bff6827aff2312092c88
sul01 <sulrend@yahoo.com>
2017-10-15 22:59:21 -0500

    Pause and MainMenu UI

---
+++ GameProjectWorking/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseUI;
+    public GameObject player;
+    private bool paused = false;
+
+    private void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            paused = !paused;
+        if(paused)
+        {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0;
+            player.GetComponent<PlayerControl>().enabled = false;
+            player.GetComponentInChildren<Gun>().enabled = false;
+        }
+        else
+        {
+            pauseUI.SetActive(false);
+            Time.timeScale = 1;
+
+            player.GetComponent<PlayerControl>().enabled = true;
+            player.GetComponentInChildren<Gun>().enabled = true;
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
