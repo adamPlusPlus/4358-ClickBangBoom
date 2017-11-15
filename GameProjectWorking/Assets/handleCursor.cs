5a99d3302300108a420144415670718fa962c065
Adam Bacchus <bacchus.adam@gmail.com>
2017-11-15 17:06:06 -0600

    psych

---
+++ GameProjectWorking/Assets/handleCursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class handleCursor : MonoBehaviour {
+
+    public Texture2D mouse;
+    public Texture2D interactObject;
+    public Texture2D interactFriendly;
+    public Texture2D interactHostile;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    private void Start()
+    {
+        setMouse();
+    }
+
+    void setMouse()
+    {
+        Cursor.SetCursor(mouse, hotSpot, cursorMode);
+    }
+
+    void setInteractObject()
+    {
+        Cursor.SetCursor(interactObject, Vector2.zero, cursorMode);
+    }
+
+    void setInteractFriendly()
+    {
+        Cursor.SetCursor(interactFriendly, Vector2.zero, cursorMode);
+    }
+
+    void setInteractHostile()
+    {
+        Cursor.SetCursor(interactHostile, Vector2.zero, cursorMode);
+    }
+}
