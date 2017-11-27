13520a7b3c0b5a4ee9fa30814a40e6cd19b6f4c2
blw <26420389+MehBerd@users.noreply.github.com>
2017-11-27 16:09:10 -0600

    Added music triggers

---
+++ GameProjectWorking/Assets/Scripts/MusicController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class MusicController: MonoBehaviour {
+  public AudioSource audioSource;
+  public AudioClip currentMusic;
+  public float initialVolume;
+  public float volume;
+  public bool loop;
+
+  public int fadeState;
+  public float fadeSpeed = 0.01f;
+  public float postFadeDelay = 0.5f;
+
+  private float delay;
+
+
+  public void FixedUpdate() {
+    switch(fadeState) {
+      case 0:  // idle, just update volume
+        audioSource.volume = volume;
+        break;
+      case 1:  // fading out
+        if(audioSource.volume > 0)
+          audioSource.volume -= fadeSpeed;
+        else {
+          audioSource.Stop();
+          fadeState = 2;
+        }
+        break;
+      case 2: // play new music
+        audioSource.volume = volume = initialVolume;
+        audioSource.loop = loop;
+        if(audioSource.clip = currentMusic)
+          audioSource.PlayDelayed(delay);
+        fadeState = 0;
+        break;
+    }
+  }
+
+  public void PlayMusic(AudioClip music, float volume = 1.0f, bool loop = true, bool fadeout = false) {
+    currentMusic = music;
+    initialVolume = volume;
+    this.loop = loop;
+    if(fadeout && audioSource.isPlaying) {// if music currently playing, fade it out first
+      delay = postFadeDelay;
+      fadeState = 1;
+    }
+    else {
+      delay = 0.0f;
+      fadeState = 2;
+    }
+  }
+}
---
+++ GameProjectWorking/Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class MusicTrigger: MonoBehaviour {
+  public MusicController musicController; 
+  public AudioClip musicClip;
+
+  public void OnTriggerEnter(Collider other) {
+    if(other.CompareTag("Player")) {
+      musicController.PlayMusic(musicClip, 1.0f, true, true);
+    }
+  }
+}
