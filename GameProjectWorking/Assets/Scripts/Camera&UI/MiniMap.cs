e6f7dcec5c078a148da5186b0d8e7622f96ec0bb
sul01 <sulrend@yahoo.com>
2017-11-30 10:31:23 -0600

    /menu and map

--- GameProjectWorking/Assets/Scripts/Camera&UI/MiniMap.cs
+++ GameProjectWorking/Assets/Scripts/Camera&UI/MiniMap.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //  Highlight player on minimap with blinking light
 //  highlight objective on minimap with blinking light
 
 public class MiniMap : MonoBehaviour {
+    
 
     void Start(){
 		
@@ -13,6 +15,9 @@ public class MiniMap : MonoBehaviour {
 	
     // Update is called once per frame
     void Update (){
-        
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<RawImage>().enabled = !GetComponent<RawImage>().enabled;
+        }
 	}
 }
