225c7547b703480d77e0ed5cba589a7e30b97ed1
maxikfu <maksimka2211@gmail.com>
2017-11-29 21:03:31 -0600

    Moose Patrol Script

--- GameProjectWorking/Assets/Scripts/Health.cs
+++ GameProjectWorking/Assets/Scripts/Health.cs
@@ -9,6 +9,7 @@ public class Health : MonoBehaviour {
     public int health;//how many hits before it dies
     public int defense;//how much it takes away from bullet's pierce
     public int scoreValue = 10;
+    public bool underAttack = false;
     private Animator anim;
     //item drop
     public bool drop = false;
@@ -16,7 +17,7 @@ public class Health : MonoBehaviour {
     public int size = 0;//size of the drops array
     public GameObject[] drops;
     private bool alreadyDrop=false;
-
+    float timerUnderAttack;
     // Use this for initialization
     void Start()
     {
@@ -26,6 +27,8 @@ public class Health : MonoBehaviour {
     public void damage(int weaponPower)//assuming weaponPower is positive
     {
         health -= weaponPower;
+        underAttack = true;
+        timerUnderAttack = 0f;
         if (health <= 0)
         {
             ScoreManager.score += scoreValue;
@@ -35,6 +38,12 @@ public class Health : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //not under attack if not getting any damage in certan time frame
+        timerUnderAttack += Time.deltaTime;
+        if (timerUnderAttack >= 10f)
+        {
+            underAttack = false;
+        }
         if (health <= 0) //handles death
         {
             // Find and disable the Nav Mesh Agent.
